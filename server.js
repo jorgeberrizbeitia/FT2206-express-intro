07f88eb1abda89e60d5d5e9a15ee8a8568820bf7
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2022-07-19 14:55:42 +0200

    params done

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/profile", (req, res) => {
+  res.send("Bienvenido a tu perfil")
+})
+
+app.get("/friends/all", (req, res) => {
+  res.send("Estos son todos mis amigos: ....")
+})
+
+app.get("/friends/:name", (req, res) => {
+  // let name = "caro"
+  console.log(req.params) // { name: "caro" }
+  // let name = req.params.name
+  const {name} = req.params
+
+  // console.log(name)
+  if (name === "caro") {
+    res.send("Esta es la información de mi amiga Caro")
+  } else if(name === "ruth") {
+    res.send("Esta es la información de mi amiga Ruth")
+  } else {
+    res.send("Lo siento, no tengo más amigos :(")
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
